d0924a6fea8defbb8b4a8fbe50158c0add4eff42
Clark Teeple <cbteeple@gmail.com>
2019-05-06 10:49:10 -0400

    Added comments to the display file

--- displayResults.py
+++ displayResults.py
@@ -6,12 +6,11 @@ plt.close('all')
 plt.rcParams.update({'font.size': 12})
 plt.rcParams['font.family'] = 'sans-serif'
 
-# Save history. 
+# Load the training history, calulate stats, then plot
 hist = np.load('hist_train.npy')
 print('\n'+"TRAINING:_____________")
 print("AVERAGE SCORE: %0.3f"%(np.mean(hist)))
 print("MAX SCORE: %0.3f"%(np.max(hist)))
-
 
 
 plt.figure()
@@ -19,11 +18,12 @@ plt.plot(hist,'o')
 plt.title('Training')
 plt.xlabel("Epoch")
 plt.ylabel("Game Score")
-
 plt.savefig('results_train.png',dpi = 300)
 plt.show()
 
 
+
+# Load the testing history, calulate stats, then plot
 hist = np.load('hist_test.npy')
 print('\n'+"FINAL TEST:_____________")
 print("AVERAGE SCORE: %0.3f"%(np.mean(hist)))
@@ -35,8 +35,5 @@ plt.plot(hist,'o')
 plt.title('Testing')
 plt.xlabel("Epoch")
 plt.ylabel("Game Score")
-
-
 plt.savefig('results_test.png',dpi = 300)
-
 plt.show()
